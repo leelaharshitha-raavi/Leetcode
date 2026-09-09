9815fcfac0c5d898313544cf04281b666465f04e
Leela Harshitha Raavi <raavileelaharshitha@gmail.com>
2026-09-09 16:56:31 +0530

    Time: 0 ms (100%), Space: 19.3 MB (46.52%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.py
+++ 3870-count-commas-in-range/3870-count-commas-in-range.py
@@ -6,9 +6,9 @@ class Solution:
             ans += n - 999
 
         if n >= 1000000:
-            ans += (n - 999999)
+            ans += 2 * (n - 999999)
 
         if n >= 1000000000:
-            ans += (n - 999999999)
+            ans += 3 * (n - 999999999)
 
         return ans
